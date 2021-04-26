c95d35e50f444b3d30a893e64ca3d69f58b1e493
moshedidi <>
2021-04-26 18:27:04 +0300

    feat - added lsearch functions

--- hw4/hw4_311395834_206202426.py
+++ hw4/hw4_311395834_206202426.py
@@ -9,6 +9,7 @@ import time
 
 import numpy as np
 
+# TODO: check input
 A = np.array([[100, 2, 3, 4, 5],
               [6, 100, 8, 9, 10],
               [11, 12, 100, 14, 15],
@@ -24,27 +25,45 @@ def gf(x):
     return 2 * ((np.dot(A.T, A)).dot(x))
 
 
-def lsearch():
-    return 1 / (2 * np.max(np.linalg.eigvals(np.dot(A.T, A))))
+''' Q1 b'''
+
+
+def const_step(s):
+    return lambda f, xk, gk: s
+
+
+def exact_quad(A):
+    np.linalg.cholesky(A)  # TODO: check
+    return lambda f, xk, gk: np.square(np.linalg.norm(gk)) / (np.dot(gk.T, A).dot(gk))
+
+
+def back(alpha, beta, s):
+    def lsearch(f, xk, gk):
+        t = s
+        while f(xk - t * gk) >= f(xk) - alpha * t * np.square(np.linalg.norm(gk)):
+            t *= beta
+        return t
+
+    return lsearch
 
 
 def generic_grad(f, gf, lsearch, x0, eps):
-    x_k = x0
-    x_k_1 = x_k - lsearch() * gf(x_k)
-    fs, gs, ts = [f(x_k)], [np.linalg.norm(gf(x_k))], [time.time()]
-    while np.abs(f(x_k) - f(x_k_1)) > eps:
-        x_k = x_k_1
-        x_k_1 = x_k - lsearch() * gf(x_k)
-        fs.append(f(x_k))
-        gs.append(np.linalg.norm(gf(x_k)))
+    xk = x0
+    xk_1 = xk - lsearch(f, xk, gf(xk)) * gf(xk)
+    fs, gs, ts = [f(xk)], [np.linalg.norm(gf(xk))], [time.time()]
+    while np.abs(f(xk) - f(xk_1)) > eps:
+        xk = xk_1
+        xk_1 = xk - lsearch(f, xk, gf(xk)) * gf(xk)
+        fs.append(f(xk))
+        gs.append(np.linalg.norm(gf(xk)))
         ts.append(time.time())
-    return x_k, fs, gs, ts
+    return xk, fs, gs, ts
 
 
 def q1():
     print(generic_grad(f,
                        gf,
-                       lsearch,
+                       back(alpha, beta, s),
                        np.array([1, 1, 1, 1, 1]),
                        1 / np.power(10, 5)))
 
