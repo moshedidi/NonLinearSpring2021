027f4d3f438302ca8ddf487cae3a2fc18c5e19f2
moshedidi <>
2021-05-05 14:34:05 +0300

    hw5 - submit

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -37,18 +37,21 @@ def exact_quad(A):
 def generic_grad(f, gf, lsearch, x0, num_of_iterations):
     xk = x0
     xk_1 = xk - lsearch(f, xk, gf(xk)) * gf(xk)
-    fs, gs, ts = [f(xk)], [np.linalg.norm(gf(xk))], [time.time()]
-    for i in range(num_of_iterations):
+    start_time = time.time()
+    fs, gs, ts = [f(xk)], [np.linalg.norm(gf(xk))], [time.time() - start_time]
+    for i in range(num_of_iterations - 1):
+        start_time = time.time()
         xk = xk_1
         xk_1 = xk - lsearch(f, xk, gf(xk)) * gf(xk)
         fs.append(f(xk))
         gs.append(np.linalg.norm(gf(xk)))
-        ts.append(time.time())
+        ts.append(time.time() - start_time + ts[-1])
     return xk, fs, gs, ts
 
 
 def q3_a():
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(10, 10)).suptitle('generic_grad', fontsize=16)
+
     ax = plt.subplot(3, 2, 1)
     plt.imshow(original_x.reshape(dimen, dimen), cmap='gray')
     ax.set_title(f'Original image')
@@ -69,7 +72,7 @@ def q3_a():
 
 
 def q3_b():
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(10, 10)).suptitle('const_step', fontsize=16)
     ax = plt.subplot(3, 2, 1)
     plt.imshow(original_x.reshape(dimen, dimen), cmap='gray')
     ax.set_title(f'Original image')
@@ -79,10 +82,10 @@ def q3_b():
     ax.set_title(f'Original blur')
 
     x0 = np.zeros(A.shape[0]).reshape(dimen * dimen, 1)
-    s = 1 / (2 * np.max(eigs(A.T.dot(A))[0]))
+    s = 1 / (2 * eigs(A.T.dot(A), k=1, return_eigenvectors=False)[0].real)
 
     for i, j in enumerate([1, 10, 100, 1000]):
-        ax = plt.subplot(3, 2, i + 1 + 2)  # the number of images in the grid is 5*5 (25)
+        ax = plt.subplot(3, 2, i + 1 + 2)
         xk, fs, gs, ts = generic_grad(f, gf, const_step(s), x0, j)
         plt.imshow(xk.reshape(dimen, dimen), cmap='gray')
         ax.set_title(f'{j} iterations')
@@ -90,10 +93,12 @@ def q3_b():
     plt.show()
 
 
-def fista(f, gf, L, xk, eps):
+def fista(f, gf, L, xk, num_of_iterations):
     yk, tk = xk, 1
-    fs, gs, ts = [f(xk)], [np.linalg.norm(gf(xk))], [time.time()]
-    while np.linalg.norm(gf(xk)) >= eps:
+    start_time = time.time()
+    fs, gs, ts = [f(xk)], [np.linalg.norm(gf(xk))], [time.time() - start_time]
+    for i in range(num_of_iterations - 1):
+        start_time = time.time()
         xk_1 = xk
         xk = yk - (1 / L) * gf(yk)
         tk_1 = tk
@@ -101,12 +106,12 @@ def fista(f, gf, L, xk, eps):
         yk = xk + ((tk_1 - 1) / tk) * (xk - xk_1)
         fs.append(f(xk))
         gs.append(np.linalg.norm(gf(xk)))
-        ts.append(time.time())
+        ts.append(time.time() - start_time + ts[-1])
     return xk, fs, gs, ts
 
 
-def q3_d():
-    plt.figure(figsize=(10, 10))
+def q3_d_1():
+    plt.figure(figsize=(10, 10)).suptitle('fista', fontsize=16)
     ax = plt.subplot(3, 2, 1)
     plt.imshow(original_x.reshape(dimen, dimen), cmap='gray')
     ax.set_title(f'Original image')
@@ -116,21 +121,58 @@ def q3_d():
     ax.set_title(f'Original blur')
 
     x0 = np.zeros(A.shape[0]).reshape(dimen * dimen, 1)
-    s = 1 / (2 * np.max(eigs(A.T.dot(A))[0]))
+    L = 2 * eigs(A.T.dot(A), k=1, return_eigenvectors=False)[0].real
 
     for i, j in enumerate([1, 10, 100, 1000]):
-        ax = plt.subplot(2, 2, i + 1 + 2)  # the number of images in the grid is 5*5 (25)
-        xk, fs, gs, ts = generic_grad(f, gf, const_step(s), x0, j)
-        xk, fs, gs, ts = fista(f, gf, L, x0, eps)
+        ax = plt.subplot(3, 2, i + 1 + 2)
+        # xk, fs, gs, ts = fista(f, gf, L, x0, eps) # TODO: what is the epsilon??
+        xk, fs, gs, ts = fista(f, gf, L, x0, j)
         plt.imshow(xk.reshape(dimen, dimen), cmap='gray')
         ax.set_title(f'{j} iterations')
 
     plt.show()
 
 
+def q3_d_2():
+    x0 = np.zeros(A.shape[0]).reshape(dimen * dimen, 1)
+
+    _exact = generic_grad(f, gf, exact_quad(A), x0, 1000)
+    L = 2 * eigs(A.T.dot(A), k=1, return_eigenvectors=False)[0].real
+
+    s = 1 / L
+    _const = generic_grad(f, gf, const_step(s), x0, 1000)
+    _fista = fista(f, gf, L, x0, 1000)
+
+    plt.semilogy(np.arange(1000), _const[1], label="Constant - f(x)")
+    plt.semilogy(np.arange(1000), _const[3], label="Constant - time")
+
+    plt.semilogy(np.arange(1000), _fista[1], label="Fista - f(x)")
+    plt.semilogy(np.arange(1000), _fista[3], label="Fista - time")
+    plt.legend()
+    plt.show()
+
+    plt.semilogy(np.arange(1000), _const[1], label="Constant - f(x)")
+    plt.semilogy(np.arange(1000), _const[3], label="Constant - time")
+
+    plt.semilogy(np.arange(1000), _exact[1], label="Exact Quad - f(x)")
+    plt.semilogy(np.arange(1000), _exact[3], label="Exact Quad - time")
+
+    plt.semilogy(np.arange(1000), _fista[1], label="Fista - f(x)")
+    plt.semilogy(np.arange(1000), _fista[3], label="Fista - time")
+    plt.legend()
+    plt.show()
+
+    plt.semilogy(_exact[3], _exact[1], label="Exact Quad - f(x)")
+    plt.semilogy(_const[3], _const[1], label="Constant - f(x)")
+    plt.legend()
+    plt.show()
+
+
 def main():
-    # q3_a()
+    q3_a()
     q3_b()
+    q3_d_1()
+    q3_d_2()
 
 
 if __name__ == '__main__':
