d25351b7f6e5c66f2542f0410b19b63574a7d19d
moshedidi <>
2021-05-12 13:26:08 +0300

    hw6 - commit and push

--- hw6/HW6_206202426_311395834.py
+++ hw6/HW6_206202426_311395834.py
@@ -5,10 +5,11 @@ Author:  moshed & eladb
 Created on 07/05/2021
 
 """
+import time
 
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy.linalg import eigvals
+from numpy.linalg import eigvals, LinAlgError
 
 
 def f(A, b):
@@ -66,12 +67,76 @@ def test_analytic_center():
     plt.show()
 
 
+""" Q2 """
+
+
+def hybrid_newton(f, gf, hf, lsearch, xk, eps):
+    fs, gs, ts, newton = [], [], [0], []
+    while np.linalg.norm(gf(xk)) > eps:
+        start_time = time.time()
+        f_xk, df_xk, hf_xk = f(xk), gf(xk), hf(xk)
+        fs.append(f_xk)
+        gs.append(df_xk)
+        try:
+            np.linalg.cholesky(hf(xk))
+            dk = np.linalg.solve(hf(xk), -df_xk)
+            tk = lsearch(xk, gf(xk), [dk, 'newton'])
+            newton.append(1)
+        except LinAlgError:
+            dk = -df_xk
+            tk = lsearch(xk, gf(xk), [dk, 'grad'])
+            newton.append(0)
+
+        xk = xk + tk * dk
+        ts.append(time.time() - start_time + ts[-1])
+    return xk, fs, gs, ts[1:], newton
+
+
+def hybrid_back(f, alpha, beta, s):
+    def lsearch(xk, gk, direction):
+        if direction[1] == 'newton':
+            return 1
+        else:
+            dk = direction[0]
+            tk = s
+
+            while f(xk + tk * dk) >= f(xk) + alpha * tk * gk.T.dot(dk):
+                tk *= beta
+            return tk
+
+    return lsearch
+
+
+def f_q2(x):
+    return x[0] ** 4 + x[1] ** 4 - 36 * x[0] * x[1]
+
+
+def df_q2(x):
+    return np.array([4 * x[0] ** 3 - 36 * x[1], 4 * x[1] ** 3 - 36 * x[0]])
+
+
+def ddf_q2(x):
+    return np.array([[12 * x[0] ** 2, -36],
+                     [- 36, 12 * x[1] ** 2]])
+
+
+def q2():
+    hn = hybrid_newton(f=f_q2,
+                       gf=df_q2,
+                       hf=ddf_q2,
+                       lsearch=hybrid_back(f=f_q2, alpha=0.25, beta=0.5, s=1),
+                       xk=np.array([200, 0]).astype('int64'),
+                       eps=10 ** -25)
+    plt.semilogy(np.arange(1, len(hn[1]) + 1), np.array(hn[1]) + 162, label="hybrid_newton")
+    plt.title("Log Scale of f(x) with respect to iteration")
+    plt.legend()
+    plt.show()
+
+
 def main():
-    # A = np.array([[2, 10]])
-    # b = np.array([0])
-    # t = f(A, b)
-    # print(np.isnan(t(np.array([1, 1]))))
-    test_analytic_center()
+    # test_analytic_center()
+    q2()
+    # x, fs, gs, ts, newton = hybrid_newton(f, gf, hf, lsearch, x0, eps)
 
 
 if __name__ == '__main__':
