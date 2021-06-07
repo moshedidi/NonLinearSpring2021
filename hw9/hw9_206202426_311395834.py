b56dd05af53a8faa379253686275473ee4709346
eladbuchris <eladbuchris@campus.technion.ac.il>
2021-06-07 11:18:38 +0300

    fixed 1a

--- hw9/hw9_206202426_311395834.py
+++ hw9/hw9_206202426_311395834.py
@@ -6,16 +6,42 @@ Created on 26/05/2021
 
 import numpy as np
 import pandas as pd
+from scipy.sparse.linalg import eigs
 import matplotlib.pyplot as plt
 
 
+def pca_project(X, k):
+    #### d ####
+    X_bar = np.zeros(X.shape[0])
+    proj = []
+    for j in range(X.shape[1]):
+        X_bar[j] = np.average(X[:, j])
+    X_centerd = (X.T - X_bar[:, np.newaxis]).T
+    XX_T = X_centerd.dot(X_centerd.T)
+    eigen = eigs(XX_T, k)[0].real
+    vectors = eigs(XX_T, k)[1].real
+    sortedEigenVectors = [x for _, x in sorted(zip(eigen, vectors), reverse=True)]
+    for vector in sortedEigenVectors:
+        for j in range(X.shape[1]):
+            proj.append(X_centerd[:, j].dot(vector))
+    np.array(proj).reshape((k,X.shape[1]))
+
+    print("good")
+
+
 def load_shares():
+    #### C ###
     prices_data = pd.read_csv('prices.csv')
     securities_data = pd.read_csv('securities.csv')
     mask = prices_data['date'].apply(lambda x: x[:4] == '2016')
     prices_2016 = prices_data[mask]
-    prices_2016[prices_2016.groupby('symbol').count('date') == 252]
-    symbols, prices, sectors = [], [[]], []
+    checkFullYear = prices_2016.groupby('symbol').count()
+    symbols = checkFullYear[checkFullYear['date'] == 252].index
+    symbolsDataFrame = symbols.to_frame(index=False)
+    sectors = pd.merge(symbolsDataFrame, securities_data, left_on='symbol', right_on='Ticker symbol', how="left")[
+        "GICS Sector"]
+    dataOnlyFull2016 = pd.merge(symbolsDataFrame, prices_2016, on='symbol', how="inner")
+    prices = [df_symbol['close'] for symbol, df_symbol in dataOnlyFull2016.groupby("symbol")]
     return symbols, prices, sectors
 
 
@@ -35,7 +61,12 @@ def q1():
 
 
 def main():
-    q1()
+    # q1()
+    X = np.array([[0, 0, 0],
+                  [3, 3, 5],
+                  [6, 8, 6],
+                  [9, 7, 9]])
+    pca_project(X, 2)
 
 
 if __name__ == '__main__':
